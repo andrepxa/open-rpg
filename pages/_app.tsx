d46d59c34046a022c0ebee310428480f87bad8ee
ericbsantana <bitencourteric@hotmail.com>
2022-01-18 22:27:45 -0300

    feat: :lipstick: add material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from '../styles/createEmotionCache'
+import Head from 'next/head'
+
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import theme from '../styles/theme'
+
+const clientSideEmotionCache = createEmotionCache()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={clientSideEmotionCache}>
+      <Head>
+        <title>MUI5 Nextjs</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
